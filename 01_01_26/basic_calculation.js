eb30d9d1325231572aa335498b0b45392836956d
krishnakalathiya <krishnakalathiya03@gmail.com>
2026-01-07 10:24:04 +0530

    01_01_26

---
+++ 01_01_26/basic_calculation.js
@@ -0,0 +1,43 @@
+{
+  var a, b;
+
+  a = 3;
+  b = 5;
+
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a / b);
+  console.log(a * b);
+  console.log(a % b);
+  console.log(a ** b);
+}
+
+/* 2. Assigment Operator */
+
+{
+  let a, b;
+  a = 5;
+  b = 4;
+
+  console.log((a += b));
+  console.log(a);
+  console.log((a -= b));
+  console.log((a /= b));
+  console.log((a *= b));
+  console.log((a %= b));
+  console.log((a **= b));
+}
+
+{
+  let x = 10;
+  let y = "10";
+
+  console.log(x == y);
+  console.log(x === y);
+  console.log(x != y);
+  console.log(x !== y);
+  console.log(x < y);
+  console.log(x > y);
+  console.log(x <= y);
+  console.log(x >= y);
+}
